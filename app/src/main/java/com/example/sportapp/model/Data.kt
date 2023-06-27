8065a171f2f142a53403f8968fb244cf8a3ab7f2
SosisochkaSGorchicey <kudrina.alice@yandex.ru>
2023-06-27 22:56:04 +0300

    fixed data classes

--- app/src/main/java/com/example/sportapp/model/Data.kt
+++ app/src/main/java/com/example/sportapp/model/Data.kt
@@ -1,10 +1,11 @@
 package com.example.sportapp.model
 
 import com.google.gson.annotations.SerializedName
+import java.io.Serializable
 
 data class SportData (
     @SerializedName("result"  ) var result  : ArrayList<Result> = arrayListOf()
-)
+): Serializable
 
 data class Result (
     @SerializedName("event_date"           ) var eventDate         : String?           = null,
@@ -18,31 +19,31 @@ data class Result (
     @SerializedName("event_home_team_logo" ) var eventHomeTeamLogo : String?           = null,
     @SerializedName("event_away_team_logo" ) var eventAwayTeamLogo : String?           = null,
     @SerializedName("scores"               ) var scores            : Scores?           = Scores(),
-)
+): Serializable
 
 data class Scores (
     @SerializedName("1stQuarter" ) var Quarter1 : ArrayList<Quarter1> = arrayListOf(),
     @SerializedName("2ndQuarter" ) var Quarter2 : ArrayList<Quarter2> = arrayListOf(),
     @SerializedName("3rdQuarter" ) var Quarter3 : ArrayList<Quarter3> = arrayListOf(),
     @SerializedName("4thQuarter" ) var Quarter4 : ArrayList<Quarter4> = arrayListOf()
-)
+): Serializable
 
 data class Quarter1 (
     @SerializedName("score_home" ) var scoreHome : String? = null,
     @SerializedName("score_away" ) var scoreAway : String? = null
-)
+): Serializable
 
 data class Quarter2 (
     @SerializedName("score_home" ) var scoreHome : String? = null,
     @SerializedName("score_away" ) var scoreAway : String? = null
-)
+): Serializable
 
 data class Quarter3 (
     @SerializedName("score_home" ) var scoreHome : String? = null,
     @SerializedName("score_away" ) var scoreAway : String? = null
-)
+): Serializable
 
 data class Quarter4 (
     @SerializedName("score_home" ) var scoreHome : String? = null,
     @SerializedName("score_away" ) var scoreAway : String? = null
-)
+): Serializable
